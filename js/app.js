f716320da7b176456c66647a2af2592a4823d24f
Nimrod51 <gavishnimrod@gmail.com>
2018-10-12 13:55:07 +0200

    initial leaflet map setup

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var const = new L.StamenTileLayer("terrain");
+
+var map = L.map('mapid', {
+	center: [51.962038,7.625937], /*Default location */
+	zoom: 15, /*Default Zoom */
+	layers: [terrain] // Default basemaplayer on startrup, can also give another layer here to show by default)
+});
+
+var baseLayers = {
+	"Terrain": terrain
+};
+
+/*
+var overlays = {
+	"Density": density
+};*/
+
+var ctrl = L.control.layers(baseLayers).addTo(map);
