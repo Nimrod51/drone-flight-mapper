ed2319d7cdcf7f5b1475767b7f31fe03b59b8184
Nimrod51 <gavishnimrod@gmail.com>
2018-10-12 16:52:22 +0200

    change to mapbox basemaps and implement remove layers function

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 /* Create basemaps */
-var terrain = new L.StamenTileLayer("terrain");
-var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-});
-var OpenStreetMap_DE = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
-  maxZoom: 18,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-});
+var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+	'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+	'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibmdhdmlzaCIsImEiOiJjaXFheHJmc2YwMDdoaHNrcWM4Yjhsa2twIn0.8i1Xxwd1XifUU98dGE9nsQ';
+
+var grayscale   = L.tileLayer(mbUrl, {id: 'mapbox.light', attribution: mbAttr}),
+streets  = L.tileLayer(mbUrl, {id: 'mapbox.streets',   attribution: mbAttr}),
+outdoors = L.tileLayer(mbUrl, {id: 'mapbox.outdoors', attribution: mbAttr}),
+satellite = L.tileLayer(mbUrl, {id: 'mapbox.satellite', attribution: mbAttr}),
+dark = L.tileLayer(mbUrl, {id: 'mapbox.dark', attribution: mbAttr}),
+light = L.tileLayer(mbUrl, {id: 'mapbox.light', attribution: mbAttr}),
+satellitestreets = L.tileLayer(mbUrl, {id: 'mapbox.streets-satellite', attribution: mbAttr});
 
 /* Initial map setup*/
 var map = L.map('mapid', {
@@ -15,7 +18,7 @@ var map = L.map('mapid', {
   /*Default location */
   zoom: 15,
   /*Default Zoom */
-  layers: [terrain] // Default basemaplayer on startrup, can also give another layer here to show by default)
+  layers: [streets] // Default basemaplayer on startrup, can also give another layer here to show by default)
 });
 
 /* Initiate variable to store flight paths drawn by user*/
@@ -31,7 +34,8 @@ var drawControl = new L.Control.Draw({
 		marker: false,
 		polyline: true,
 		rectangle: false,
-		circle: false
+		circle: false,
+		circlemarker: false
 	},
   edit: {
     featureGroup: flightItems
@@ -40,23 +44,32 @@ var drawControl = new L.Control.Draw({
 map.addControl(drawControl);
 
 
-// /* Store items in the 'flightItems' variable upon finishing */
+/* Store items in the 'flightItems' variable upon finishing */
+var count=0;
 map.on(L.Draw.Event.CREATED, function(event) {
   var layer = event.layer;
   flightItems.addLayer(layer);
+	ctrl.addOverlay(layer, count);
+	count+=1
 });
 
+/* When user removes a layer AND saves changes then remove all layers from legend */
+map.on(L.Draw.Event.DELETED, function(event) {
+	layers = event.layers._layers
+	for (var layerIndex in layers) {
+        ctrl.removeLayer(layers[layerIndex]);
+}
 
-/* Store all  basemaps in variable  */
+});
+
 var baseLayers = {
-  "Terrain": terrain,
-  "OpenStreetMap_Mapnik": OpenStreetMap_Mapnik,
-  "OpenStreetMap DE": OpenStreetMap_DE
-};
-
-/*
-var overlays = {
-	"Density": density
-};*/
+		"Grayscale": grayscale,
+		"Streets": streets,
+		"Outdoors": outdoors,
+		"Satellite": satellite,
+		"Satellite Streets": satellitestreets,
+		"Dark Map": dark,
+		"Light Map": light
+	};
 
 var ctrl = L.control.layers(baseLayers).addTo(map);
